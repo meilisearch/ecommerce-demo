26abc824255d87349610f1d47032ed38c0cc9426
Strift <laurent@meilisearch.com>
2023-01-31 10:48:21 +0100

    Update vite configuration for Storybook to work with Nuxt auto-imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,25 @@
 import { URL, fileURLToPath } from 'node:url'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
 
 import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [
+    vue(),
+    AutoImport({
+      imports: ['vue', 'vue-router'],
+      dirs: ['./composables'],
+      vueTemplate: true
+    }),
+    Components({
+      dirs: ['./components'],
+      dts: true,
+      directoryAsNamespace: true
+    })
+  ],
   resolve: {
     alias: {
       '~': fileURLToPath(new URL('./', import.meta.url))
