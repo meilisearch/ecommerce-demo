8b62c20243c084f2b8eabb5457f818d46529b459
Strift <laurent@meilisearch.com>
2023-02-27 12:00:16 +0100

    Update Pagination to display first and last pages

--- components/organisms/MeiliSearchPagination.vue
+++ components/organisms/MeiliSearchPagination.vue
@@ -8,27 +8,43 @@ import { AisPagination } from 'vue-instantsearch/vue3/es'
       #default="{
         currentRefinement,
         pages,
-        refine
+        refine,
+        nbPages,
+        isFirstPage,
+        isLastPage
       }"
     >
-      <BaseTitle>
-        <span
-          v-for="page in pages"
-          :key="page"
+      <BaseTitle v-if="!isFirstPage && !pages.includes(0)" tag="span">
+        <a href="#" class="link" @click.prevent="refine(0)">
+          Page 1
+        </a>
+        <span class="text-ashes-900"> &bull;<span v-if="!pages.includes(1)">&bull;&bull;</span> </span>
+      </BaseTitle>
+      <!-- Current page and 3 previous/next -->
+      <BaseTitle
+        v-for="(page, index) in pages"
+        :key="page"
+        tag="span"
+      >
+        <a
+          href="#"
+          :class="{'active': page === currentRefinement}"
+          class="link"
+          @click.prevent="refine(page)"
         >
-          <a
-            href="#"
-            :class="{'active': page === currentRefinement}"
-            class="link"
-            @click.prevent="refine(page)"
-          >
-            Page {{ page + 1 }}
-          </a>
-          <span
-            v-if="page < (pages.length-1)"
-            class="text-ashes-900"
-          > &bull; </span>
-        </span>
+          Page {{ page + 1 }}
+        </a>
+        <span
+          v-if="index < (pages.length-1)"
+          class="text-ashes-900"
+        > &bull; </span>
+      </BaseTitle>
+      <!-- Last page -->
+      <BaseTitle v-if="!isLastPage && !pages.includes(nbPages-1)" tag="span">
+        <span class="text-ashes-900"> &bull;<span v-if="!pages.includes(nbPages-2)">&bull;&bull;</span> </span>
+        <a href="#" class="link" @click.prevent="refine(nbPages-1)">
+          Page {{ nbPages }}
+        </a>
       </BaseTitle>
     </template>
   </ais-pagination>
