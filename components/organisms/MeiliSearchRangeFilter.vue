9c8872ae2dca56add643187641e681ec17ce4e52
Strift <laurent@meilisearch.com>
2023-03-02 17:45:21 +0100

    Use RangeSlider in MeiliSearchRangeFilter

--- components/organisms/MeiliSearchRangeFilter.vue
+++ components/organisms/MeiliSearchRangeFilter.vue
@@ -1,8 +1,5 @@
 <script lang="ts" setup>
 import { AisRangeInput } from 'vue-instantsearch/vue3/es'
-import VueSlider from 'vue-slider-component/dist-css/vue-slider-component.umd.min.js'
-import 'vue-slider-component/dist-css/vue-slider-component.css'
-import 'vue-slider-component/theme/default.css'
 
 interface Range {
   min: number
@@ -44,22 +41,11 @@ const toValue = (currentValue: Range, boundaries: Range): [number, number] => {
           <span class="text-ashes-900">$ </span>{{ currentRefinement.max ?? range.max }}
         </BaseText>
       </div>
-      <VueSlider
+      <RangeSlider
         :model-value="toValue(currentRefinement, range)"
         :min="range.min"
         :max="range.max"
-        :lazy="true"
-        tooltip="hover"
-        class="body"
-        :tooltip-style="{
-          background: 'var(--ashes-600)',
-          borderColor : 'var(--ashes-600)',
-          color: 'var(--valhalla-500)',
-          padding: 'var(--size-2)'
-        }"
-        :process-style="{ background: 'var(--dodger-blue-500)' }"
-        :tooltip-formatter="(value: number) => `$ ${value}`"
-        @change="refine({ min: $event[0], max: $event[1] })"
+        @update:model-value="refine($event)"
       />
     </template>
   </ais-range-input>
