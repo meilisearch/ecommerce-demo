738e82cfed0a10c2391ae1a7aca9f84047ddd658
Strift <laurent@meilisearch.com>
2023-03-13 17:21:55 +0100

    Comment code using section 2 components

--- components/templates/HomeTemplate.vue
+++ components/templates/HomeTemplate.vue
@@ -8,29 +8,29 @@ const sortingOptions = [
 </script>
 
 <template>
-  <MeiliSearchProvider index-name="products">
-    <TheNavbar class="mb-5 shadow-l">
-      <template #search>
-        <MeiliSearchBar />
-      </template>
-    </TheNavbar>
-    <div class="container mb-5">
-      <div class="filters">
-        <!-- <MeiliSearchFacetFilter attribute="category" class="mb-5" /> -->
-        <!-- <MeiliSearchFacetFilter attribute="brand" class="mb-5" /> -->
-        <!-- <MeiliSearchRangeFilter attribute="price" class="mb-5" /> -->
-        <!-- <MeiliSearchRatingFilter attribute="rating_rounded" label="Rating" /> -->
-      </div>
-      <div class="results">
-        <div class="mb-5 results-meta">
-          <MeiliSearchStats />
-          <!-- <MeiliSearchSorting :options="sortingOptions" /> -->
-        </div>
-        <MeiliSearchResults class="mb-5" />
-        <!-- <MeiliSearchPagination /> -->
-      </div>
+  <!-- <MeiliSearchProvider index-name="products"> -->
+  <TheNavbar class="mb-5 shadow-l">
+    <template #search>
+      <!-- <MeiliSearchBar /> -->
+    </template>
+  </TheNavbar>
+  <div class="container mb-5">
+    <div class="filters">
+      <!-- <MeiliSearchFacetFilter attribute="category" class="mb-5" /> -->
+      <!-- <MeiliSearchFacetFilter attribute="brand" class="mb-5" /> -->
+      <!-- <MeiliSearchRangeFilter attribute="price" class="mb-5" /> -->
+      <!-- <MeiliSearchRatingFilter attribute="rating_rounded" label="Rating" /> -->
     </div>
-  </MeiliSearchProvider>
+    <div class="results">
+      <div class="mb-5 results-meta">
+        <!-- <MeiliSearchStats /> -->
+        <!-- <MeiliSearchSorting :options="sortingOptions" /> -->
+      </div>
+      <!-- <MeiliSearchResults class="mb-5" /> -->
+      <!-- <MeiliSearchPagination /> -->
+    </div>
+  </div>
+  <!-- </MeiliSearchProvider> -->
 </template>
 
 <style scoped>
