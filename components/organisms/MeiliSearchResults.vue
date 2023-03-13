c4e2092bdfae813e1f3eb31da91e52492d933072
Strift <laurent@meilisearch.com>
2023-03-13 17:16:27 +0100

    Remove MeiliSearchResults code

--- components/organisms/MeiliSearchResults.vue
+++ components/organisms/MeiliSearchResults.vue
@@ -1,24 +1,10 @@
 <script lang="ts" setup>
 import { AisHits } from 'vue-instantsearch/vue3/es'
+import ProductCard from '~/components/organisms/ProductCard.vue'
 </script>
 
 <template>
-  <AisHits>
-    <template #default="{ items }">
-      <div class="items">
-        <ProductCard
-          v-for="product in items"
-          :key="product.id"
-          :name="product.title"
-          :brand="product.brand"
-          :price="product.price"
-          :image-url="product.images[0]"
-          :rating="product.rating"
-          :reviews-count="product.reviews_count"
-        />
-      </div>
-    </template>
-  </AisHits>
+  <!-- Insert template code here  -->
 </template>
 
 <style scoped>
