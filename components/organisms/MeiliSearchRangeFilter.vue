fa8949b70b7c100b964f30912c8227a3faf628bc
Strift <laurent@meilisearch.com>
2023-03-13 16:47:19 +0100

    Remove MeiliSearchRangeFilter code

--- components/organisms/MeiliSearchRangeFilter.vue
+++ components/organisms/MeiliSearchRangeFilter.vue
@@ -1,54 +1,14 @@
 <script lang="ts" setup>
 import { AisRangeInput } from 'vue-instantsearch/vue3/es'
+import BaseTitle from '~/components/atoms/BaseTitle.vue'
+import BaseText from '~/components/atoms/BaseText.vue'
+import RangeSlider from '~/components/molecules/RangeSlider.vue'
 
-interface Range {
-  min: number
-  max: number
-}
-
-const props = defineProps<{
-  attribute: string
-}>()
-
-const minPrice = 0
-const maxPrice = 1900
-
-const { attribute } = toRefs(props)
-
-const toValue = (currentValue: Range, boundaries: Range): [number, number] => {
-  return [
-    typeof currentValue.min === 'number' ? currentValue.min : boundaries.min,
-    typeof currentValue.max === 'number' ? currentValue.max : boundaries.max
-  ]
-}
+// Insert script code here
 </script>
 
 <template>
-  <AisRangeInput
-    :attribute="attribute"
-    :min="minPrice"
-    :max="maxPrice"
-  >
-    <template #default="{ currentRefinement, range, refine }">
-      <BaseTitle class="mb-3 text-valhalla-100">
-        {{ attribute }}
-      </BaseTitle>
-      <div class="slider-labels text-valhalla-500 mb-2">
-        <BaseText size="m">
-          <span class="text-ashes-900">$ </span>{{ currentRefinement.min ?? range.min }}
-        </BaseText>
-        <BaseText size="m">
-          <span class="text-ashes-900">$ </span>{{ currentRefinement.max ?? range.max }}
-        </BaseText>
-      </div>
-      <RangeSlider
-        :model-value="toValue(currentRefinement, range)"
-        :min="range.min"
-        :max="range.max"
-        @update:model-value="refine($event)"
-      />
-    </template>
-  </AisRangeInput>
+  <!-- Insert template code here -->
 </template>
 
 <style scoped>
