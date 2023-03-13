4613b8e78892cc1ec732f3bfb2e16c703c494833
Strift <laurent@meilisearch.com>
2023-03-13 17:25:03 +0100

    Remove code from section 1

--- database/setup.js
+++ database/setup.js
@@ -25,38 +25,7 @@ const setup = async () => {
     process.exit(1)
   }
 
-  const client = new MeiliSearch(credentials)
-
-  console.log(`Adding filterable attributes to \`${INDEX_NAME}\``)
-  await client.index(INDEX_NAME).updateFilterableAttributes([
-    'brand',
-    'category',
-    'tag',
-    'rating_rounded',
-    'reviews_count',
-    'price'
-  ])
-
-  console.log(`Adding ranking rules to \`${INDEX_NAME}\``)
-  await client.index(INDEX_NAME).updateRankingRules([
-    'sort',
-    'words',
-    'typo',
-    'proximity',
-    'attribute',
-    'exactness'
-  ])
-
-  console.log(`Adding sortable attributes to \`${INDEX_NAME}\``)
-  await client.index(INDEX_NAME).updateSortableAttributes([
-    'rating',
-    'price'
-  ])
-
-  console.log(`Adding documents to \`${INDEX_NAME}\``)
-  await client.index(INDEX_NAME).addDocuments(data)
-
-  await watchTasks(client, INDEX_NAME)
+  // Setup code will go here
 }
 
 setup()
