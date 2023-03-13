ead76be3373788850d8e4599bc0887bcdad05778
Strift <laurent@meilisearch.com>
2023-03-13 16:55:25 +0100

    Remove MeiliSearchRatingFilter code

--- components/organisms/MeiliSearchRatingFilter.vue
+++ components/organisms/MeiliSearchRatingFilter.vue
@@ -1,47 +1,14 @@
 <script lang="ts" setup>
 import { AisRatingMenu } from 'vue-instantsearch/vue3/es'
+import BaseTitle from '~/components/atoms/BaseTitle.vue'
+import BaseText from '~/components/atoms/BaseText.vue'
+import StarRating from '~/components/molecules/StarRating.vue'
 
-const props = defineProps<{
-  attribute: string
-  label?: string
-}>()
-
-const { attribute } = toRefs(props)
+// Insert script code here
 </script>
 
 <template>
-  <AisRatingMenu
-    :attribute="attribute"
-    :max="5"
-  >
-    <template #default="{ items, refine }">
-      <BaseTitle class="mb-3 text-valhalla-100">
-        {{ label ?? attribute }}
-      </BaseTitle>
-      <a
-        v-for="item in items"
-        :key="item.value"
-        class="rating-link"
-        :class="[item.isRefined ? 'text-dodger-500' : 'text-valhalla-500']"
-        href="#"
-        @click.prevent="refine(item.value)"
-      >
-        <span class="rating-label">
-          <StarRating :rating="Number(item.value)" />
-          <BaseText
-            tag="span"
-            size="m"
-            class="ml-1"
-          >
-            & Up
-            <BaseText tag="span" size="s" class="text-ashes-900">
-              ({{ item.count.toLocaleString() }})
-            </BaseText>
-          </BaseText>
-        </span>
-      </a>
-    </template>
-  </AisRatingMenu>
+  <!-- Insert template code here -->
 </template>
 
 <style scoped>
