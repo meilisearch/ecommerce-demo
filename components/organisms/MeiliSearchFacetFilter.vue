d4e05570ca4cfa698f40de91fd8fe65a80ee0caa
Strift <laurent@meilisearch.com>
2023-03-13 16:45:50 +0100

    Remove MeiliSearchFacetFilter code

--- components/organisms/MeiliSearchFacetFilter.vue
+++ components/organisms/MeiliSearchFacetFilter.vue
@@ -1,37 +1,12 @@
 <script lang="ts" setup>
 import { AisRefinementList } from 'vue-instantsearch/vue3/es'
+import BaseTitle from '~/components/atoms/BaseTitle.vue'
+import BaseText from '~/components/atoms/BaseText.vue'
+import BaseCheckbox from '~/components/atoms/BaseCheckbox.vue'
 
-const props = defineProps<{
-  attribute: string
-}>()
-
-const { attribute } = toRefs(props)
+// Insert script code here
 </script>
 
 <template>
-  <AisRefinementList
-    :attribute="attribute"
-    operator="or"
-  >
-    <template #default="{ items, refine }">
-      <BaseTitle class="mb-3 text-valhalla-100">
-        {{ attribute }}
-      </BaseTitle>
-      <BaseCheckbox
-        v-for="item in items"
-        :key="item.value"
-        :value="item.isRefined"
-        :label="item.label"
-        :name="item.value"
-        :disabled="item.count === 0"
-        @change="refine(item.value)"
-      >
-        <BaseText tag="span" size="m" :class="[ item.count ? 'text-valhalla-500' : 'text-ashes-900']">
-          {{ item.label }} <BaseText tag="span" size="s" class="text-ashes-900">
-            ({{ item.count.toLocaleString() }})
-          </BaseText>
-        </BaseText>
-      </BaseCheckbox>
-    </template>
-  </AisRefinementList>
+  <!-- Insert template code here -->
 </template>
