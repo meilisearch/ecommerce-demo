b8602594e03afc5ea21e046e77cc56bc5930ec11
Strift <laurent@meilisearch.com>
2023-07-12 23:14:49 +0200

    Add search for facet values

--- components/organisms/MeiliSearchFacetFilter.vue
+++ components/organisms/MeiliSearchFacetFilter.vue
@@ -1,37 +1,79 @@
 <script lang="ts" setup>
 import { AisRefinementList } from 'vue-instantsearch/vue3/es'
 
+type SortingOrder = 'isRefined' | 'count' | 'name'
+
 const props = defineProps<{
   attribute: string
+  sortBy: SortingOrder[]
 }>()
 
 const { attribute } = toRefs(props)
+
+const searchInput = ref<string>('')
+
+const refineFacet = (searchFn: any, inputValue: string) => {
+  searchInput.value = inputValue
+  searchFn(inputValue)
+}
 </script>
 
 <template>
   <AisRefinementList
     :attribute="attribute"
+    :searchable="true"
+    :show-more="true"
+    :sort-by="sortBy"
     operator="or"
   >
-    <template #default="{ items, refine }">
+    <template
+      #default="{ items, refine, searchForItems, isFromSearch, isShowingMore, canToggleShowMore, toggleShowMore }"
+    >
       <BaseTitle class="mb-3 text-valhalla-100">
         {{ attribute }}
       </BaseTitle>
-      <BaseCheckbox
-        v-for="item in items"
-        :key="item.value"
-        :value="item.isRefined"
-        :label="item.label"
-        :name="item.value"
-        :disabled="item.count === 0"
-        @change="refine(item.value)"
-      >
-        <BaseText tag="span" size="m" :class="[ item.count ? 'text-valhalla-500' : 'text-ashes-900']">
-          {{ item.label }} <BaseText tag="span" size="s" class="text-ashes-900">
-            ({{ item.count.toLocaleString() }})
-          </BaseText>
+      <SearchInput
+        class="mb-3"
+        :value="searchInput"
+        @input="refineFacet(searchForItems, $event.target.value)"
+        @reset="refineFacet(searchForItems, '')"
+      />
+      <div class="mb-3">
+        <BaseText v-if="isFromSearch && !items.length" tag="span" size="m" class="text-ashes-900">
+          No results.
         </BaseText>
-      </BaseCheckbox>
+        <BaseCheckbox
+          v-for="item in items"
+          :key="item.value"
+          :value="item.isRefined"
+          :label="item.label"
+          :name="item.value"
+          :disabled="item.count === 0"
+          @change="refine(item.value)"
+        >
+          <BaseText tag="span" size="m" :class="[ item.count ? 'text-valhalla-500' : 'text-ashes-900']">
+            {{ item.label }} <BaseText tag="span" size="s" class="text-ashes-900">
+              ({{ item.count.toLocaleString() }})
+            </BaseText>
+          </BaseText>
+        </BaseCheckbox>
+      </div>
+      <BaseTitle v-show="canToggleShowMore" tag="span">
+        <a
+          href="#"
+          class="link"
+          @click.prevent="toggleShowMore"
+        >
+          {{ !isShowingMore ? 'Show more' : 'Show less' }}
+        </a>
+      </BaseTitle>
     </template>
   </AisRefinementList>
 </template>
+
+<style scoped>
+.link {
+  text-decoration: none;
+  color: var(--ashes-900);
+}
+</style>
--- components/templates/HomeTemplate.vue
+++ components/templates/HomeTemplate.vue
@@ -16,8 +16,8 @@ const sortingOptions = [
     </TheNavbar>
     <div class="container mb-5">
       <div class="filters">
-        <MeiliSearchFacetFilter attribute="category" class="mb-5" />
-        <MeiliSearchFacetFilter attribute="brand" class="mb-5" />
+        <MeiliSearchFacetFilter attribute="category" :sort-by="['isRefined', 'name']" class="mb-5" />
+        <MeiliSearchFacetFilter attribute="brand" :sort-by="['isRefined', 'count']" class="mb-5" />
         <MeiliSearchRangeFilter attribute="price" class="mb-5" />
         <MeiliSearchRatingFilter attribute="rating_rounded" label="Rating" />
       </div>
